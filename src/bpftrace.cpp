e34d9a44c8437cf2300985ed54ce0658c32f6b58
Omid Azizi <oazizi@pixielabs.ai>
2021-11-10 15:40:48 -0800

    deploy/poll/finalize model

--- src/bpftrace.cpp
+++ src/bpftrace.cpp
@@ -1074,7 +1074,7 @@ int BPFtrace::prerun() const
   return 0;
 }
 
-int BPFtrace::run(BpfBytecode bytecode)
+int BPFtrace::deploy(BpfBytecode bytecode)
 {
   int err = prerun();
   if (err)
@@ -1180,17 +1180,11 @@ int BPFtrace::run(BpfBytecode bytecode)
   if (std::getenv("__BPFTRACE_NOTIFY_PROBES_ATTACHED"))
     std::cerr << "__BPFTRACE_NOTIFY_PROBES_ATTACHED" << std::endl;
 
-  if (has_iter_)
-  {
-    int err = run_iter();
-    if (err)
-      return err;
-  }
-  else
-  {
-    poll_perf_events();
-  }
+  return 0;
+}
 
+int BPFtrace::finalize()
+{
   attached_probes_.clear();
   // finalize_ and exitsig_recv should be false from now on otherwise
   // perf_event_printer() can ignore the END_trigger() events.
@@ -1204,6 +1198,32 @@ int BPFtrace::run(BpfBytecode bytecode)
 
   // Calls perf_reader_free() on all open perf buffers.
   open_perf_buffers_.clear();
+
+  return 0;
+}
+
+int BPFtrace::run(BpfBytecode bytecode)
+{
+  int err = 0;
+
+  err = deploy(std::move(bytecode));
+  if (err) 
+    return err;
+
+  if (has_iter_)
+  {
+    int err = run_iter();
+    if (err)
+      return err;
+  }
+  else
+  {
+    poll_perf_events();
+  }
+
+  err = finalize();
+  if (err)
+    return err;
 
   return 0;
 }
