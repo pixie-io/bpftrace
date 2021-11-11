f13f43b4c32e5c24c3fb26e77aa07026e9d56e7f
Omid Azizi <oazizi@pixielabs.ai>
2021-11-10 16:09:14 -0800

    poll_perf_buffers: make epollfd_ member variable and expose timeout

--- src/bpftrace.cpp
+++ src/bpftrace.cpp
@@ -1249,12 +1249,9 @@ int BPFtrace::setup_perf_events()
   return 0;
 }
 
-void BPFtrace::poll_perf_events(bool drain)
+void BPFtrace::poll_perf_events(bool drain, int timeout_ms)
 {
-  const int timeout_ms = 100;
-
-  if (epollfd_ < 0)
-  {
+  if (epollfd_ < 0) {
     LOG(ERROR) << "Invalid epollfd " << epollfd_;
     return;
   }
